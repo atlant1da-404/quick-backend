279798e9c86c2c3f714e7920a34dab8cab68ee13
montana4x4 <ilusha2k03@gmail.com>
2026-03-04 23:23:45 +0200

    feat(general): add sync pool

--- internal/infra/handler/rest/note.go
+++ internal/infra/handler/rest/note.go
@@ -17,6 +17,19 @@ type createNoteRequestBody struct {
 	Title string `json:"title"`
 }
 
+var notePool = sync.Pool{
+	New: func() any {
+		return &model.NoteCreate{}
+	},
+}
+
+var batchPool = sync.Pool{
+	New: func() any {
+		b := make([]*model.NoteCreate, 0, 1000) // заміни на свій maxBatch
+		return &b
+	},
+}
+
 const noteRequestLimit = 512
 
 func (a *apiHandler) CreateNote(ctx *fasthttp.RequestCtx) error {
@@ -43,9 +56,20 @@ func (a *apiHandler) CreateNote(ctx *fasthttp.RequestCtx) error {
 		return nil
 	}
 
+	// беремо Note з пулу
+	note := notePool.Get().(*model.NoteCreate)
+	note.Id = id
+	note.Title = req.Title
+
 	select {
-	case a.flushChan <- &model.NoteCreate{Id: id, Title: req.Title}:
+	case a.flushChan <- note:
+		// все ок
 	default:
+		// канал переповнений, повертаємо Note в пул
+		note.Id = ""
+		note.Title = ""
+		notePool.Put(note)
+
 		ctx.SetStatusCode(fasthttp.StatusServiceUnavailable)
 		return nil
 	}
@@ -57,13 +81,18 @@ func (a *apiHandler) CreateNote(ctx *fasthttp.RequestCtx) error {
 func (a *apiHandler) worker(ctx context.Context, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	batch := make([]*model.NoteCreate, 0, a.maxBatch)
+	// беремо батч з пулу
+	batchPtr := batchPool.Get().(*[]*model.NoteCreate)
+	batch := *batchPtr
+	batch = batch[:0] // очищаємо перед використанням
+
 	ticker := time.NewTicker(a.flushDur)
 	defer ticker.Stop()
 
 	for {
 		select {
 		case <-ctx.Done():
+			// дренимо канал і flush перед виходом
 			for {
 				select {
 				case note := <-a.flushChan:
@@ -76,13 +105,15 @@ func (a *apiHandler) worker(ctx context.Context, wg *sync.WaitGroup) {
 					if len(batch) > 0 {
 						a.flush(batch)
 					}
+					// повертаємо батч в пул
+					*batchPtr = batch[:0] // очистимо перед поверненням
+					batchPool.Put(batchPtr)
 					return
 				}
 			}
 
 		case note := <-a.flushChan:
 			batch = append(batch, note)
-
 			if len(batch) >= a.maxBatch {
 				a.flush(batch)
 				batch = batch[:0]
@@ -99,4 +130,10 @@ func (a *apiHandler) worker(ctx context.Context, wg *sync.WaitGroup) {
 
 func (a *apiHandler) flush(notes []*model.NoteCreate) {
 	a.uc.CreateNotesBatch(notes)
+
+	for _, note := range notes {
+		note.Id = ""
+		note.Title = ""
+		notePool.Put(note)
+	}
 }
