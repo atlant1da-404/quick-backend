d3450c5f9e48f17ad3788f960efb58fc05431ddf
montana4x4 <ilusha2k03@gmail.com>
2026-03-05 21:16:29 +0200

    feat(general): fix data race

--- internal/usecase/note.go
+++ internal/usecase/note.go
@@ -2,7 +2,6 @@ package usecase
 
 import (
 	"github.com/atlant1da-404/internal/model"
-	"sync"
 )
 
 func (u usecase) CreateNotesBatch(notes []*model.NoteCreate) {
@@ -10,34 +9,10 @@ func (u usecase) CreateNotesBatch(notes []*model.NoteCreate) {
 		return
 	}
 
-	workers := 4
-	if len(notes) < workers {
-		workers = len(notes)
+	for _, n := range notes {
+		u.cache.CreateNote(&model.Note{
+			Id:    n.Id,
+			Title: n.Title,
+		})
 	}
-
-	chunkSize := (len(notes) + workers - 1) / workers
-
-	var wg sync.WaitGroup
-	wg.Add(workers)
-
-	for i := 0; i < workers; i++ {
-		start := i * chunkSize
-		end := start + chunkSize
-		if start >= len(notes) {
-			wg.Done() // нічого немає для цього worker
-			continue
-		}
-		if end > len(notes) {
-			end = len(notes)
-		}
-
-		go func(subNotes []*model.NoteCreate) {
-			defer wg.Done()
-			for _, n := range subNotes {
-				u.cache.CreateNote(&model.Note{Id: n.Id, Title: n.Title})
-			}
-		}(notes[start:end])
-	}
-
-	wg.Wait()
 }
